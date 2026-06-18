c4d1b2dceee0547645090c19a3f8524cbce4897f
nabil <nwardana122@gmail.com>
2026-06-19 01:37:13 +0700

    fix: ui

--- app/src/main/java/com/example/communityeventmanagementsystem/presentation/event/SavedEventsScreen.kt
+++ app/src/main/java/com/example/communityeventmanagementsystem/presentation/event/SavedEventsScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.EventBusy
@@ -19,13 +18,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import coil.compose.AsyncImage
 import com.example.communityeventmanagementsystem.ui.theme.*
 import com.example.communityeventmanagementsystem.core.common.DateTimeUtils
 
--- app/src/main/java/com/example/communityeventmanagementsystem/presentation/notifications/NotificationScreen.kt
+++ app/src/main/java/com/example/communityeventmanagementsystem/presentation/notifications/NotificationScreen.kt
@@ -1,13 +1,11 @@
 package com.example.communityeventmanagementsystem.presentation.notifications
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.NotificationsActive
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
